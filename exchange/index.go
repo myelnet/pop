2c74b74ce16780707a2a9a0b6aa77f5689ba6f94
gallexis <alexis.gallepe@gmail.com>
2021-08-06 16:55:10 +0200

    use raw cid instead of DagCBOR

--- exchange/index.go
+++ exchange/index.go
@@ -534,7 +534,7 @@ func (idx *Index) CleanBlockStore(ctx context.Context) error {
 	cidSet := cid.NewSet()
 
 	err := utils.WalkDAG(ctx, idx.rootCID, idx.bstore, sel.All(), func(blk blocks.Block) error {
-		key := cid.NewCidV1(cid.DagCBOR, blk.Cid().Hash())
+		key := cid.NewCidV1(cid.Raw, blk.Cid().Hash())
 		cidSet.Add(key)
 		return nil
 	})
@@ -547,8 +547,7 @@ func (idx *Index) CleanBlockStore(ctx context.Context) error {
 		return err
 	}
 	for k := range kc {
-		key := cid.NewCidV1(cid.DagCBOR, k.Hash())
-		if cidSet.Has(key) {
+		if cidSet.Has(k) {
 			continue
 		}
 		err = idx.Bstore().DeleteBlock(k)
