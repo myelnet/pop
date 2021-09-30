d565f1cff47650fbd80f1584e99d3ad0df8f6282
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2021-09-30 20:43:47 +0100

    feat: webhook listener for automatically updating pop
    * lightweight server for listening to Github webhooks and fetching new pop releases accordingly.
    * fixed arm64 cross-compilation using goreleaser.
    * renamed build directory to infra

---
+++ infra/k8s/install-playbook/influxdb-env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export INFLUXDB_URL="http://influxdb:8086"
+export INFLUXDB_TOKEN="v_mfrI166vkpkfA2REXzo7kAxqxP4Vh97dXexExBGP4VVpDax-uWBy93qliZ0qaZCDyUvfS3PovTcq5fxhxiXA=="
+export INFLUXDB_ORG="Myel"
+export INFLUXDB_BUCKET="retrievals"
