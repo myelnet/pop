3ecb6d1bd74e9907533dbf9962998e1cd10d1c78
tchardin <tdotchardin@gmail.com>
2021-11-06 13:02:20 +0000

    fix: bcli routing issue

--- cmd/bcli/app.js
+++ cmd/bcli/app.js
@@ -66,7 +66,7 @@ function checkValidSW(url) {
 }
 
 window.onload = function () {
-  checkValidSW('/static/sw.js');
+  checkValidSW('/sw.js');
   navigator.serviceWorker.ready.then(() => {
     console.log(
       'This web app is serving content from a Myel client in a service worker'
