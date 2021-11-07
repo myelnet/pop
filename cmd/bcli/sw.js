bc6802cc35b3f2d2909379e20e8d297dc09b0f3a
tchardin <tdotchardin@gmail.com>
2021-11-07 12:46:41 +0000

    chore: update sw client with directory listing support

--- cmd/bcli/sw.js
+++ cmd/bcli/sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f12f5d3df2e4cfb8997feaa646e5ac86883dc36630ae2fc8bff09fd6537dc48
-size 1012776
+oid sha256:accc85e1682dacba4a78d7eb1c1fd8b58288912832a48b50764c7bc67ec9eeba
+size 1012962
