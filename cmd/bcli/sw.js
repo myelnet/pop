cdc7debb9636bdf0acf2e44fb8cdd03524c45c43
tchardin <tdotchardin@gmail.com>
2021-11-07 08:55:54 +0000

    chore: update sw client in benchmark cli

--- cmd/bcli/sw.js
+++ cmd/bcli/sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e58d760598893edef98346acb629abb2ef813a0b5908a372f5f81de76cb2d5f
-size 1008643
+oid sha256:f9e12b3e28cfb1db9209dab0fdf81d60c34a9306b0fb1e4e68cd6b9eb34a68e2
+size 1008846
