44373a531ca49c2a9ecdd5f480d6bb802b4eda0c
tchardin <tdotchardin@gmail.com>
2021-11-07 12:12:27 +0000

    chore: update sw client

--- cmd/bcli/sw.js
+++ cmd/bcli/sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50446b480d0e04c69c2895c38ae62a738c92c448b3ad4e4c661c688a15a30494
-size 1012770
+oid sha256:8f12f5d3df2e4cfb8997feaa646e5ac86883dc36630ae2fc8bff09fd6537dc48
+size 1012776
