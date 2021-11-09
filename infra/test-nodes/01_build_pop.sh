2f400e1ba50e7e58d1589daf24e5ba4806674bdf
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2021-11-09 11:51:49 +0000

    fix: make sure all env variables set all the time

--- infra/test-nodes/01_build_pop.sh
+++ infra/test-nodes/01_build_pop.sh
@@ -16,13 +16,9 @@ START_TIME=`date +%s`
 echo "Building base image for Myel pops..."
 echo
 
-REGISTRY_URL=$1
-
-if [ -z "$REGISTRY_URL" ]
-then
-  echo -e "Please provide a docker registry url. For example: \`./01_build.sh aws.com/my-registry\`"
-  exit 2
-fi
+source "$my_dir/install-playbook/cluster-env.sh"
+source "$my_dir/install-playbook/influxdb-env.sh"
+source "$my_dir/install-playbook/validation.sh"
 
 docker build --platform linux/x86_64 -t ${REGISTRY_URL}:latest .
 # uncomment if using aws registry
